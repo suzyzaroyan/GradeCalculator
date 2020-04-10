372775314856baef31d0ac50d877d8ac192e78b9
suzyzaroyan <62140748+suzyzaroyan@users.noreply.github.com>
2020-04-10 22:56:08 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function calculateGrade() {
+
+  var homework_percent = parseInt(document.getElementById ("homework-percent").innerHTML)
+
+  var homework_grade = parseFloat(document.getElementById ("hwGrade").value)
+
+  homework = homework_percent * homework_grade/100
+
+  var participation_percent = parseInt(document.getElementById ("participation-percent").innerHTML)
+
+  var participation_grade = parseFloat(document.getElementById ("participation-grade").value)
+
+  participation = participation_percent * participation_grade/100
+
+  var quiz_percent = parseInt(document.getElementById ("quiz-percent").innerHTML)
+
+  var quiz_grade = parseFloat(document.getElementById ("quiz-grade").value)
+
+  quiz = quiz_percent * quiz_grade/100
+
+  var finalproject_percent = parseInt(document.getElementById ("finalproject-percent").innerHTML)
+
+  var finalproject_grade = parseFloat(document.getElementById ("finalproject-grade").value)
+
+  finalproject = finalproject_percent * finalproject_grade/100
+
+  var currentGrade = homework + participation + quiz + finalproject
+  console.log(currentGrade)
+
+  document.getElementById("currentGradeVAL").innerHTML= currentGrade
+}
