2ecf636365aa72c37cc1a0eb175ea43281725f7e
suzyzaroyan <62140748+suzyzaroyan@users.noreply.github.com>
2020-04-20 00:06:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,37 @@ function calculateGrade() {
 
   document.getElementById("currentGradeVAL").innerHTML= currentGrade
 }
+
+
+function LetterGrade() {
+      var homework_percent =parseInt(document.getElementById('homework-percent').innerHTML);
+            var participation_percent=parseInt(document.getElementById('participation-percent').innerHTML);
+            var quiz_percent=parseInt(document.getElementById('quiz-percent').innerHTML);
+            var finalproject_percent=parseInt(document.getElementById("finalproject-percent").innerHTML);
+            
+
+
+            var homework_grade =parseFloat(document.getElementById('hwGrade').value);
+            var participation_grade =parseFloat(document.getElementById('participation-grade').value);
+            var quiz_grade =parseFloat(document.getElementById('quiz-grade').value);
+            var finalproject_grade =parseFloat(document.getElementById('finalproject-grade').value);
+
+
+            var Current_Grade = homework_percent*homework_grade/100 + participation_percent*participation_grade/100 + quiz_percent*quiz_grade/100 + finalproject_percent*finalproject_grade/100 
+            
+      
+      
+      if (Current_Grade <= 100 && Current_Grade >=90) {letter_grade = "A"}
+
+      if (Current_Grade < 90 && Current_Grade >=80) {letter_grade = "B"}
+
+      if (Current_Grade < 80 && Current_Grade >=70) {letter_grade = "C"}
+
+      if (Current_Grade < 70  && Current_Grade>=60 ){letter_grade = "D"}
+      
+      if (Current_Grade < 60) {letter_grade = "F"}
+
+      var display=document.getElementById('LetterGrade');
+            display.innerHTML=' Current Letter Grade: ' + letter_grade;
+      
+      }
